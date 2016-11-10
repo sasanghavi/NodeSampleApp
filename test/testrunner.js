574d15d461dc633b2d754a5fb9e5926c2f803d57
Shrey Sanghavi <ssangha@ncsu.edu>
2016-11-10 17:05:57 -0500

    Test added

---
+++ test/testrunner.js
@@ -0,0 +1,21 @@
+var request = require("request");
+
+var base_url = "http://159.203.120.47:3000/"
+
+describe("basic server", function() {
+  describe("GET /", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("returns hello world", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(body).toBe("Hello world");
+        done();
+      });
+    });
+  });
+});
